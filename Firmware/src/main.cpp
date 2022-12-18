cf389b76559b9ec192f570614190c37436a4dbda
haythem-cpu <haythemjaidane11@gmail.com>
2022-12-18 19:55:32 +0100

    deploy the model in nodeMCU

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,9 +1,30 @@
 #include <Arduino.h>
+#include "model.h"
+
+float size ;
+
+Eloquent::ML::Port::DecisionTree Tree_Model;
 
 void setup() {
+
+  randomSeed(analogRead(0));
+
   Serial.begin(9600);
+  Serial.println("*********************************");
+  Serial.println("Welcome to my first TinyML");
+  Serial.println("*********************************");
+
+  
 }
 
 void loop() {
-  Serial.println("Hello World !");
+  
+  size=random(300);
+
+  Serial.print("the size is ");
+  Serial.print(size);
+  Serial.print("the decission is ");
+  Serial.println(Tree_Model.predictLabel(&size));
+
+  delay(2000);
 }
